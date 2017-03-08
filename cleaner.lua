83b0f6571bb5547b2dcf6dd4be727f88eec1feb4
sajjad_021 <sajjad-021@users.noreply.github.com>
2017-03-08 17:39:28 +0330

    Create cleaner.lua

---
+++ cleaner.lua
@@ -0,0 +1,7 @@
+redis = (loadfile "redis.lua")()
+io.write("Enter Tabchi ID : ")
+local last = io.read()
+io.popen('rm -rf ~/.telegram-cli/tabchi-'..last..' tabchi-'..last..'.lua tabchi-'..last..'.sh tabchi_'..last..'_logs.txt')
+redis:del('tabchi:'..last..':*')
+print("Done!\nAll Data/Files Of Tabchi Deleted\nTabchi ID : "..last)
+print('Cracked Version 4 Of Tabchi By @sajjad_021 ;D - @tgMember ;o  Good Luck :D')
