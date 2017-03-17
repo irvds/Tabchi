b08ae135699190bd1e5a3e5d17e64bf918a146cf
sajjad_021 <sajjad-021@users.noreply.github.com>
2017-03-17 03:10:20 -0700

    Update anticrash.sh

--- anticrash.sh
+++ anticrash.sh
@@ -1,45 +1,4 @@
 #!/bin/bash
-  declare -A logo
-    seconds="0.014"
-logo[-1]=" ::::::::::  :::::::      ::     ::  ::::::::  ::     ::  ::::::  :::::::: ::::::  "
-logo[0]="     +:     :+    :+:    :+:+   +::+ +:       :+:+   +:+: +:   :+ +:       +:   :+ "
-logo[1]="     :+     +:           :+ +:+:+ :+ :+       :+ +:+:+ :+ :+   +: :+       :+   +: "
-logo[2]="     ++     :#           ++  +:+  ++ +++++#   ++  +:+  ++ #+++++  +++:+#   +++++#  "
-logo[3]="     ++     +#  +#+#+    #+   +   #+ #+       #+   +   +# #+   +# #+       #+   +# "
-logo[4]="     +#     #+     +#    +#       +# +#       +#       #+ +#    # +#       +#    #+"
-logo[5]="     ##      #######     ##       ## ######## ##       ## ####### ######## ##    ##"
-    printf "\033[32;4;600m\t"
-    for i in ${!logo[@]}; do
-        for x in `seq 0 ${#logo[$i]}`; do
-            printf "${logo[$i]:$x:1}"
-            sleep $seconds
-        done
-        printf "\n\t"
-    done
-printf "\n"
-rm -rf ~/.telegram-cli/tabchi-*/data/photo
-rm -rf ~/.telegram-cli/tabchi-*/data/animation
-rm -rf ~/.telegram-cli/tabchi-*/data/audio
-rm -rf ~/.telegram-cli/tabchi-*/data/document
-rm -rf ~/.telegram-cli/tabchi-*/data/sticker
-rm -rf ~/.telegram-cli/tabchi-*/data/temp
-rm -rf ~/.telegram-cli/tabchi-*/data/video
-rm -rf ~/.telegram-cli/tabchi-*/data/voice
-rm -rf ~/.telegram-cli/tabchi-*/data/profile_photo
-rm -rf ~/.telegram-cli/tabchi-*/data/thumb
-sudo apt-get install -y tor
-sudo service tor start
-sudo export http_proxy=socks5://127.0.0.1:9150 https_proxy=socks5://127.0.0.1:9150
-sudo apt-get autoclean
-sudo apt-get autoremove
-sudo apt-get update
-sudo apt-get upgrade
-sudo apt-add-repository --remove ppa:ubuntu-toolchain-r/test
-sudo apt-get install -f
-sudo dpkg -a --configure
-sudo apt-get dist-upgrade
-sudo dpkg --configure -a
-sudo sudo apt-get dist-upgrade     
 while true ; do
   for entr in tabchi-*.sh ; do
     entry="${entr/.sh/}"
@@ -53,19 +12,19 @@ while true ; do
     rm -rf ~/.telegram-cli/$entry/data/video/*
     rm -rf ~/.telegram-cli/$entry/data/voice/*
     rm -rf ~/.telegram-cli/$entry/data/profile_photo/*
+    rm -rf ~/.telegram-cli/tabchi-*/data/photo
+    rm -rf ~/.telegram-cli/tabchi-*/data/animation
+    rm -rf ~/.telegram-cli/tabchi-*/data/audio
+    rm -rf ~/.telegram-cli/tabchi-*/data/document
+    rm -rf ~/.telegram-cli/tabchi-*/data/sticker
+    rm -rf ~/.telegram-cli/tabchi-*/data/temp
+    rm -rf ~/.telegram-cli/tabchi-*/data/video
+    rm -rf ~/.telegram-cli/tabchi-*/data/voice
+    rm -rf ~/.telegram-cli/tabchi-*/data/profile_photo
+    rm -rf ~/.telegram-cli/tabchi-*/data/thumb
     tmux new-session -d -s $entry "./$entr"
     tmux detach -s $entry
   done
   echo Bots Running!
   sleep 1800
 done
-echo -e "\033[36;7;208m"
-   echo -e " :::::::::::  ::::::::     ::::    ::::  :::::::::: ::::    ::::  :::::::::  :::::::::: ::::::::: "
-   echo -e "     :+:     :+:    :+:    +:+:+: :+:+:+ :+:        +:+:+: :+:+:+ :+:    :+: :+:        :+:    :+:"
-   echo -e "     +:+     +:+           +:+ +:+:+ +:+ +:+        +:+ +:+:+ +:+ +:+    +:+ +:+        +:+    +:+"
-   echo -e "     +#+     :#:           +#+  +:+  +#+ +#++:++#   +#+  +:+  +#+ +#++:++#+  +#++:++#   +#++:++#: "
-   echo -e "     +#+     +#+   +#+#    +#+       +#+ +#+        +#+       +#+ +#+    +#+ +#+        +#+    +#+"
-   echo -e "     #+#     #+#    #+#    #+#       #+# #+#        #+#       #+# #+#    #+# #+#        #+#    #+#"
-   echo -e "     ###      ########     ###       ### ########## ###       ### #########  ########## ###    ###"   
-   echo -e " ------------------------------------------------------------------------------------------------ "
-   echo -e "\033[32M;5;600m"
